d6f1f82995f3a23969446eb6d601016f2493ab9e
James Sanders <jsanders@comverge.com>
2010-05-30 16:04:48 -0600

    Redo examples - dead simple echo server, and one that logs everything

---
+++ examples/echo_server.js
@@ -0,0 +1,3 @@
+var sys = require('sys');
+var snpp = require('../lib/snpp');
+snpp.createServer().listen('444', 'localhost');
--- examples/log_server.js
+++ examples/log_server.js
@@ -1,30 +1,21 @@
 var sys = require('sys');
 var snpp = require('../lib/snpp');
-var server = snpp.createServer();
 
-var pager, message;
+function logWithSocket(message, socket) { sys.log(message + ' for ' + socket.remoteAddress + ':' + socket.remotePort); }
+snpp.createServer(function (req, res) {
+  req.addListener('page', function () {
+    logWithSocket('Handling PAGE command for pager: ' + req.pager, req.socket);
+  });
 
-server.addListener('page', function (_pager) {
-  pager = _pager;
-  sys.log('Handling PAGE command for pager: ' + pager);
-  server.outputStatusMessage(250);
-});
+  req.addListener('mess', function () {
+    logWithSocket('Handling MESS command for message: ' + req.message, req.socket);
+  });
 
-server.addListener('mess', function (_message) {
-  message = _message;
-  sys.log('Handling MESS command with message: ' + message);
-  server.outputStatusMessage(250);
-});
+  req.addListener('send', function () {
+    logWithSocket('Sending message "' + req.message + '" to pager at ' + req.pager, req.socket);
+  });
 
-server.addListener('send', function () {
-  sys.log('Sending message "' + message + '" to pager at ' + pager);
-  server.outputStatusMessage(250);
-});
-
-server.addListener('quit', function () {
-  sys.log('Quitting');
-  server.outputStatusMessage(221);
-  server.closeConnection();
-});
-
-server.listen('444', 'localhost');
+  req.addListener('quit', function () {
+    logWithSocket('Quitting', req.socket);
+  });
+}).listen('444', 'localhost');
